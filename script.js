2217f86b3db0974f1d8750b358835e53ad824578
uriel levy <uriellevy4892@gmail.com>
2021-09-21 13:11:39 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,65 @@
+////////HEADER RESPONSIVE NAVBAR
+
+const links = document.querySelectorAll("nav ul li a");
+const icon = document.getElementById("icon");
+const navShow = document.querySelector("nav ul");
+
+links.forEach((link) => {
+  link.addEventListener("click", function () {
+    removeClass();
+
+    link.classList.add("active");
+  });
+});
+
+function removeClass() {
+  links.forEach((cur) => {
+    cur.classList.remove("active");
+  });
+}
+
+icon.addEventListener("click", function () {
+  navShow.classList.toggle("show");
+});
+
+//////animation
+
+window.addEventListener("scroll", reveal);
+
+function reveal() {
+  const reveals = document.querySelectorAll(".reveal");
+
+  reveals.forEach((cur) => {
+    const windowHeight = window.innerHeight;
+    const revealTop = cur.getBoundingClientRect().top;
+    const revealPoint = 150;
+
+    if (revealTop < windowHeight - revealPoint) {
+      cur.classList.add("active");
+    } else {
+      cur.classList.remove("active");
+    }
+  });
+}
+
+/////sticky navbar
+const nav = document.querySelector("nav");
+window.addEventListener("scroll", function () {
+  nav.classList.toggle("sticky", window.scrollY > 400);
+});
+
+///implement google maps
+
+function initMap() {
+  const loc = { lat: 9.158984, lng: -83.743792 };
+
+  const map = new google.maps.Map(document.querySelector(".map"), {
+    zoom: 14,
+    center: loc,
+  });
+
+  const marker = new google.maps.Marker({
+    position: loc,
+    map: map,
+  });
+}
